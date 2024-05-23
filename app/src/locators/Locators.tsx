5b4d5cc153cbeb435ce83ca17437e0c5990a0524
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2024-05-23 11:07:17 +0200

    Delete PlayerColor to Locators

--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -1,6 +1,6 @@
 import { LocationType } from '@gamepark/arctic/material/LocationType'
 import { MaterialType } from '@gamepark/arctic/material/MaterialType'
-import { PlayerColor } from '@gamepark/arctic/PlayerColor'
+import { PlayerId } from '@gamepark/arctic/PlayerId'
 import { ItemLocator } from '@gamepark/react-game'
 import { animalCardsDeckLocator } from './AnimalCardsDeckLocator'
 import { reserveLocator } from './ReserveLocator'
@@ -13,7 +13,7 @@ import { riverLocator } from './RiverLocator'
 import { animalPileLocator } from './AnimalPileLocator'
 import { powerPileLocator } from './PowerPileLocator'
 
-export const Locators: Partial<Record<LocationType, ItemLocator<PlayerColor, MaterialType, LocationType>>> = {
+export const Locators: Partial<Record<LocationType, ItemLocator<PlayerId, MaterialType, LocationType>>> = {
   [LocationType.TableCenter]: tableCenterLocator,
   [LocationType.AnimalCardsDeck]: animalCardsDeckLocator,
   [LocationType.Reserve]: reserveLocator,
