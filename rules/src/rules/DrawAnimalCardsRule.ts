b74ecf993c102652908b702df9e9ef182f27f3e1
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2024-05-23 14:47:27 +0200

    Minor change in DrawAnimalCardRule

--- rules/src/rules/DrawAnimalCardsRule.ts
+++ rules/src/rules/DrawAnimalCardsRule.ts
@@ -1,13 +1,14 @@
 import { ItemMove, MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
 import { LocationType } from '../material/LocationType'
 import { MaterialType } from '../material/MaterialType'
+import { Memory } from './Memory'
 import { RuleId } from './RuleId'
 
 export class DrawAnimalCardsRule extends PlayerTurnRule {
   onRuleStart() {
-    const pileCardsId = this.material(MaterialType.AnimalCard).location(LocationType.AnimalPile).player(this.player).getItems().map(item => item.id)
-    const drawValue = parseInt(pileCardsId[pileCardsId.length - 1].toString().slice(-1), 10)
-    this.memorize('Draw value', drawValue, this.player)
+    const topPileCard = this.material(MaterialType.AnimalCard).location(LocationType.AnimalPile).player(this.player).sort(item => -item.location.x!).getItem()
+    const drawValue = topPileCard!.id % 10
+    this.memorize<number>(Memory.DrawValue, drawValue)
     return []
   }
 
@@ -17,8 +18,8 @@ export class DrawAnimalCardsRule extends PlayerTurnRule {
   }
 
   afterItemMove(_move: ItemMove): MaterialMove[] {
-    this.memorize('Draw value', this.remind('Draw value', this.player) - 1, this.player)
-    if (this.remind('Draw value', this.player) == 0) {
+    this.memorize<number>(Memory.DrawValue, value => value - 1)
+    if (this.remind<number>(Memory.DrawValue) == 0) {
       const missingCards = 6 - this.material(MaterialType.AnimalCard).location(LocationType.River).length
 
       return [
--- rules/src/rules/Memory.ts
+++ rules/src/rules/Memory.ts
@@ -1,3 +1,4 @@
 export enum Memory {
   DepositValue,
+  DrawValue,
 }
