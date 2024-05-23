5e72af0c602c381674a8cfbe09f0b2410a59d73a
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2024-05-23 10:45:06 +0200

    Refactor rules

--- rules/src/rules/DrawAnimalCardsRule.ts
+++ rules/src/rules/DrawAnimalCardsRule.ts
@@ -4,22 +4,27 @@ import { MaterialType } from '../material/MaterialType'
 import { RuleId } from './RuleId'
 
 export class DrawAnimalCardsRule extends PlayerTurnRule {
+  onRuleStart() {
+    const pileCardsId = this.material(MaterialType.AnimalCard).location(LocationType.AnimalPile).player(this.player).getItems().map(item => item.id)
+    const drawValue = parseInt(pileCardsId[pileCardsId.length - 1].toString().slice(-1), 10)
+    this.memorize('Draw value', drawValue, this.player)
+    return []
+  }
+
   getPlayerMoves() {
     return this.material(MaterialType.AnimalCard).location(LocationType.River)
       .moveItems({ type: LocationType.PlayerHand, player: this.player })
   }
 
-  onRuleStart() {
-    const pileCardsId = this.material(MaterialType.AnimalCard).location(LocationType.AnimalPile).player(this.player).getItems().map(item => item.id)
-    const valueDeposit = parseInt(pileCardsId[pileCardsId.length - 1].toString().slice(-1), 10)
-    this.memorize('Draw value', valueDeposit, this.player)
-    return []
-  }
-
   afterItemMove(_move: ItemMove): MaterialMove[] {
     this.memorize('Draw value', this.remind('Draw value', this.player) - 1, this.player)
     if (this.remind('Draw value', this.player) == 0) {
-      return [this.rules().startPlayerTurn(RuleId.PlayAnimalCards, this.nextPlayer)]
+      const missingCards = 6 - this.material(MaterialType.AnimalCard).location(LocationType.River).length
+
+      return [
+        ...this.material(MaterialType.AnimalCard).location(LocationType.AnimalCardsDeck).deck().deal({ type: LocationType.River }, missingCards),
+        this.rules().startPlayerTurn(RuleId.PlayAnimalCards, this.nextPlayer)
+      ]
     }
     return []
   }
--- rules/src/rules/PlayAnimalCardsRule.ts
+++ rules/src/rules/PlayAnimalCardsRule.ts
@@ -1,47 +1,42 @@
 import { isMoveItemType, ItemMove, MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
 import { LocationType } from '../material/LocationType'
 import { MaterialType } from '../material/MaterialType'
-import { RuleId } from './RuleId';
+import { PowerCard } from '../material/PowerCard'
+import { Memory } from './Memory'
+import { RuleId } from './RuleId'
 
 export class PlayAnimalCardsRule extends PlayerTurnRule {
-  getPlayerMoves() {
-    return this.material(MaterialType.AnimalCard).location(LocationType.PlayerHand).player(this.player)
-      .moveItems({ type: LocationType.AnimalPile, player: this.player })
-  }
-
   onRuleStart() {
-    if (this.remind('Value deposit', this.player) === undefined) {
-      this.memorize('Value deposit', 1, this.player);
+    const topPileCard = this.material(MaterialType.AnimalCard).location(LocationType.AnimalPile).player(this.player).sort(item => -item.location.x!).getItem()
+
+    if (!topPileCard) {
+      this.memorize<number>(Memory.DepositValue, 1)
     }
     else {
-      const pileCardsId = this.material(MaterialType.AnimalCard).location(LocationType.AnimalPile).player(this.player).getItems().map(item => item.id)
-      const depositValue = 6 - parseInt(pileCardsId[pileCardsId.length - 1].toString().slice(-1), 10)
-      this.memorize('Value deposit', depositValue, this.player)
+      const depositValue = 6 - (topPileCard.id % 10)
+      this.memorize<number>(Memory.DepositValue, depositValue)
     }
 
     return []
   }
 
+  getPlayerMoves() {
+    return this.material(MaterialType.AnimalCard).location(LocationType.PlayerHand).player(this.player)
+      .moveItems({ type: LocationType.AnimalPile, player: this.player })
+  }
+
   afterItemMove(move: ItemMove): MaterialMove[] {
-    const powerCardsIds = this.material(MaterialType.PowerCard).getItems().map(item => item.id);
+    if (isMoveItemType(MaterialType.AnimalCard)(move) && move.location.type === LocationType.AnimalPile) {
+      this.memorize<number>(Memory.DepositValue, value => value - 1);
 
-    if (isMoveItemType(MaterialType.AnimalCard)(move)) {
-      const animalId = Math.floor(this.material(MaterialType.AnimalCard).getItem(move.itemIndex)?.id / 100);
-
-      if (animalId !== undefined) {
-        const powerCardId = powerCardsIds.find(power => power.toString().startsWith(animalId.toString()));
-
-        if (powerCardId !== undefined) {
-          return this.material(MaterialType.PowerCard).id(powerCardId)
-            .moveItems({ type: LocationType.PowerPile, player: this.player });
-        }
+      if (this.remind<number>(Memory.DepositValue) == 0) {
+        const animalId = Math.floor(this.material(MaterialType.AnimalCard).getItem(move.itemIndex)!.id / 100);
+        const powerCard = this.material(MaterialType.PowerCard).id<PowerCard>(id => Math.floor(id / 10) === animalId)
+        return [
+          powerCard.moveItem({ type: LocationType.PowerPile, player: this.player }),
+          this.rules().startRule(RuleId.DrawAnimalCards)
+        ]
       }
-    }
-
-    this.memorize('Value deposit', this.remind('Value deposit', this.player) - 1, this.player);
-
-    if (this.remind('Value deposit', this.player) == 0) {
-      return [this.rules().startRule(RuleId.DrawAnimalCards)];
     }
 
     return []
