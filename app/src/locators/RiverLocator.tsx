fc842fcbfbd716db593d489578aef79cf5fb20cd
Patrick Beaume <p.beaume@gmail.com>
2024-08-22 17:46:49 +0200

    add scale on hover for river

--- app/src/locators/RiverLocator.tsx
+++ app/src/locators/RiverLocator.tsx
@@ -6,24 +6,28 @@ import { animalCardDescription } from '../material/AnimalCardDescription'
 import { RiverDescription } from './descriptions/RiverDescription'
 
 class RiverLocator extends ListLocator {
-    locationDescription = new RiverDescription()
-    coordinates = { x: -20, y: 4, z: 0 }
-    gap = { x: animalCardDescription.width + 1, y: 0 }
+  locationDescription = new RiverDescription()
+  coordinates = { x: -20, y: 4, z: 0 }
+  gap = { x: animalCardDescription.width + 1, y: 0 }
 
-    getLocations(context: MaterialContext): Location[] {
-        const { rules } = context
-        const river = rules.material(MaterialType.AnimalCard).location(LocationType.River).getItems()
-        return river.map((r) => r.location)
+  getLocations(context: MaterialContext): Location[] {
+    const { rules } = context
+    const river = rules.material(MaterialType.AnimalCard).location(LocationType.River).getItems()
+    return river.map((r) => r.location)
+  }
+
+  getCoordinates(_location: Location, context: MaterialContext) {
+    if (context.player || context.rules.players.length === 2) return this.coordinates
+    return {
+      ...this.coordinates,
+      x: this.coordinates.x + 8
     }
 
-    getCoordinates(_location: Location, context: MaterialContext) {
-        if (context.player || context.rules.players.length === 2) return this.coordinates
-        return {
-            ...this.coordinates,
-            x: this.coordinates.x + 8
-        }
+  }
 
-    }
+  getHoverTransform(): string[] {
+    return ['translateZ(10em)', 'scale(2)', 'translateY(-25%)']
+  }
 }
 
 export const riverLocator = new RiverLocator()
