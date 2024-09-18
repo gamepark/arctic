0fb7453ae546f10f1fc6680cc42f39630a5fecd1
Romain Fromi <romain.fromi@gmail.com>
2024-09-18 15:29:45 +0200

    Option to customize each power in friendly games

--- rules/src/ArcticOptions.ts
+++ rules/src/ArcticOptions.ts
@@ -1,4 +1,6 @@
-import { OptionsSpec } from '@gamepark/rules-api'
+import { Option, OptionsSpec } from '@gamepark/rules-api'
+import { Animal } from './material/Animal'
+import { PowerCard } from './material/PowerCard'
 
 /**
  * This is the type of object that the game receives when a new game is started.
@@ -6,11 +8,53 @@ import { OptionsSpec } from '@gamepark/rules-api'
  */
 export type ArcticOptions = {
   players: number
+  bear: PowerCard.Bear1 | PowerCard.Bear2
+  fox: PowerCard.Fox1 | PowerCard.Fox2
+  moose: PowerCard.Moose1 | PowerCard.Moose2
+  orca: PowerCard.Orca1 | PowerCard.Orca2
+  puffin: PowerCard.Puffin1 | PowerCard.Puffin2
+  walrus: PowerCard.Walrus1 | PowerCard.Walrus2
 }
+
+const powerValueSpec = (power: PowerCard): Option => ({
+  label: t => t('power.number', { number: power % 10 }),
+  help: t => t(`power.${power}`),
+  competitiveDisabled: true
+})
 
 /**
  * This object describes all the options a game can have, and will be used by GamePark website to create automatically forms for you game
  * (forms for friendly games, or forms for matchmaking preferences, for instance).
  */
 export const ArcticOptionsSpec: OptionsSpec<ArcticOptions> = {
+  bear: {
+    label: t => t(`power.animal.${Animal.Bear}`),
+    values: [PowerCard.Bear1, PowerCard.Bear2],
+    valueSpec: powerValueSpec
+  },
+  fox: {
+    label: t => t(`power.animal.${Animal.Fox}`),
+    values: [PowerCard.Fox1, PowerCard.Fox2],
+    valueSpec: powerValueSpec
+  },
+  moose: {
+    label: t => t(`power.animal.${Animal.Moose}`),
+    values: [PowerCard.Moose1, PowerCard.Moose2],
+    valueSpec: powerValueSpec
+  },
+  orca: {
+    label: t => t(`power.animal.${Animal.Orca}`),
+    values: [PowerCard.Orca1, PowerCard.Orca2],
+    valueSpec: powerValueSpec
+  },
+  puffin: {
+    label: t => t(`power.animal.${Animal.Puffin}`),
+    values: [PowerCard.Puffin1, PowerCard.Puffin2],
+    valueSpec: powerValueSpec
+  },
+  walrus: {
+    label: t => t(`power.animal.${Animal.Walrus}`),
+    values: [PowerCard.Walrus1, PowerCard.Walrus2],
+    valueSpec: powerValueSpec
+  }
 }
